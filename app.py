47444f6e8f15aeeba242a3b15cc8543776c1e635
SPECTRE <spectre@osint.dev>
2026-09-10 16:02:14 -0500

    Bust browser cache and add strict anti-caching headers

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
     return response
 
 def get_param(key):
